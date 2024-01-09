b7bdaac12b35c2d2b6cca33f4b90fe43df65311b
Денис Тычин <dencat87@gmail.com>
2024-01-09 23:40:52 +0700

    Fix remarks

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,31 +1,22 @@
-from django.urls import path
-from .views import (ApiPostListCreateView,
-                    ApiPostDetailView,
-                    ApiGroupListView,
-                    ApiGroupDetailView,
-                    ApiCommentListCreateView,
-                    ApiCommentDetailView)
+from django.urls import path, include
+
 from rest_framework.authtoken import views
+from rest_framework.routers import DefaultRouter
+
+from api.views import (CommentViewSet,
+                       GroupViewSet,
+                       PostViewSet)
+
+
+router_v1 = DefaultRouter()
+
+router_v1.register('posts', PostViewSet, basename='posts')
+router_v1.register('posts/(?P<post_id>\\d+)/comments', CommentViewSet,
+                basename='comments')
+router_v1.register('groups', GroupViewSet, basename='groups')
 
 urlpatterns = [
-    path('api-token-auth/',
+    path('v1/api-token-auth/',
          views.obtain_auth_token),
-    path('posts/',
-         ApiPostListCreateView.as_view(),
-         name='api-post-list-create'),
-    path('posts/<int:pk>/',
-         ApiPostDetailView.as_view(),
-         name='api-post-detail'),
-    path('groups/',
-         ApiGroupListView.as_view(),
-         name='api-group-list'),
-    path('groups/<int:pk>/',
-         ApiGroupDetailView.as_view(),
-         name='api-group-detail'),
-    path('posts/<int:post_id>/comments/',
-         ApiCommentListCreateView.as_view(),
-         name='api-comment-list-create'),
-    path('posts/<int:post_id>/comments/<int:comment_id>/',
-         ApiCommentDetailView.as_view(),
-         name='api-comment-detail'),
+    path('v1/', include(router_v1.urls)),
 ]
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,91 +1,39 @@
-from rest_framework import generics, status
-from rest_framework.authentication import TokenAuthentication
-from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
-from posts.models import Post, Comment, Group
-from .serializers import (ApiPostSerializer,
-                          ApiCommentSerializer,
-                          GroupSerializer)
+from rest_framework import permissions
+from rest_framework import viewsets
+
+from api.permissions import IsAuthorOrReadOnly
+from posts.models import Post, Group
+from api.serializers import (PostSerializer,
+                             CommentSerializer,
+                             GroupSerializer)
 
 
-class ApiPostListCreateView(generics.ListCreateAPIView):
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
-    serializer_class = ApiPostSerializer
-    authentication_classes = [TokenAuthentication]
+    serializer_class = PostSerializer
+    permission_classes = (
+        permissions.IsAuthenticated, IsAuthorOrReadOnly)
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
 
-class ApiPostDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Post.objects.all()
-    serializer_class = ApiPostSerializer
-    authentication_classes = [TokenAuthentication]
-
-    def destroy(self, request, *args, **kwargs):
-        instance = self.get_object()
-        if request.user != instance.author:
-            return Response({"detail": "Не трогай чужое"},
-                            status=status.HTTP_403_FORBIDDEN)
-        self.perform_destroy(instance)
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-    def update(self, request, *args, **kwargs):
-        instance = self.get_object()
-        if request.user != instance.author:
-            return Response({"detail": "Не трогай чужое"},
-                            status=status.HTTP_403_FORBIDDEN)
-        return super().update(request, *args, **kwargs)
-
-
-class ApiGroupListView(generics.ListAPIView):
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
-    authentication_classes = [TokenAuthentication]
-
-
-class ApiGroupDetailView(generics.RetrieveAPIView):
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
-    authentication_classes = [TokenAuthentication]
-
-
-class ApiCommentListCreateView(generics.ListCreateAPIView):
-    queryset = Comment.objects.all()
-    serializer_class = ApiCommentSerializer
-    authentication_classes = [TokenAuthentication]
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = (permissions.IsAuthenticated,
+                          IsAuthorOrReadOnly)
 
     def get_queryset(self):
-        post_id = self.kwargs['post_id']
-        return Comment.objects.filter(post_id=post_id)
+        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+        return post.comments
 
     def perform_create(self, serializer):
-        post_id = self.request.data.get('post_id')
-        if not post_id:
-            post_id = self.kwargs.get('post_id')
-        post = None
-        if post_id:
-            post = Post.objects.get(pk=post_id)
+        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
         serializer.save(author=self.request.user, post=post)
-
-
-class ApiCommentDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Comment.objects.all()
-    serializer_class = ApiCommentSerializer
-    authentication_classes = [TokenAuthentication]
-    lookup_url_kwarg = 'comment_id'
-
-    def update(self, request, *args, **kwargs):
-        instance = self.get_object()
-        if request.user != instance.author:
-            return Response({"detail": "Не трогай чужое"},
-                            status=status.HTTP_403_FORBIDDEN)
-        return super().update(request, *args, **kwargs)
-
-    def destroy(self, request, *args, **kwargs):
-        instance = self.get_object()
-        if request.user != instance.author:
-            return Response({"detail": "Не трогай чужое"},
-                            status=status.HTTP_403_FORBIDDEN)
-        self.perform_destroy(instance)
-        return Response(status=status.HTTP_204_NO_CONTENT)
